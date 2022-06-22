6a6b1da626a8fd9e97756ddec63efa3d3b104542
Truong-Vo <vqt.dnn@gmail.com>
2022-06-22 16:52:29 +0700

    second
    Signed-off-by: Truong-Vo <vqt.dnn@gmail.com>

---
+++ src/components/AccountItem/AccountItem.js
@@ -0,0 +1,30 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/8fb454410a01457f65edeb3886221bbf~c5_300x300.webp?x-expires=1656061200&x-signature=4023uQqwU7PpGsN6K8T3yuutwFw%3D"
+                alt="Hoaa"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>nanaxinhxan19</span>
+                    <FontAwesomeIcon
+                        className={cx('check')}
+                        icon={faCheckCircle}
+                    />
+                </h4>
+                <span className={cx('username')}>Nguyễn Phúc Kim Ngân</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
--- src/components/Layouts/HeaderOnly/HeaderOnly.js
+++ src/components/Layouts/HeaderOnly/HeaderOnly.js
@@ -1,4 +1,4 @@
-import Header from '~/components/Layouts/DefaultLayout/Header/Header';
+import Header from '~/components/Layouts/components/Header/Header';
 
 function HeaderOnly({ children }) {
     return (
